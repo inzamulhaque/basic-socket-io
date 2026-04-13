5f1522ee1588cb99edcfb31f623386b8a0482694
inzamulhaque <mdihalif@gmail.com>
2026-04-13 16:14:51 +0600

    part-6

--- socket/orderHandler.js
+++ socket/orderHandler.js
@@ -22,17 +22,17 @@ export const orderHandler = (io, socket) => {
       }
 
       const totals = calculateTotal(data.items);
-      const orderID = generateOrderId();
-      const orderData = createOrderDocument(data, orderID, totals);
+      const orderId = generateOrderId();
+      const orderData = createOrderDocument(data, orderId, totals);
 
       const ordersCollection = getCollection("orders");
       await ordersCollection.insertOne(orderData);
 
       // Emit order confirmation to the client
-      socket.join(orderID); // Join a room with the order ID
+      socket.join(orderId); // Join a room with the order ID
       socket.join("customers"); // Join a common room for all customers
 
-      socket.join("admin").emit("newOrder", { orderData }); // Notify admin about new order
+      socket.join("admins").emit("newOrder", { orderData }); // Notify admin about new order
 
       callback({
         success: true,
@@ -46,6 +46,34 @@ export const orderHandler = (io, socket) => {
       callback({
         success: false,
         message: "Failed to place order",
+      });
+    }
+  });
+
+  // track order status
+  socket.on("trackOrder", async (data, callback) => {
+    try {
+      const ordersCollection = getCollection("orders");
+      const order = await ordersCollection.findOne({ orderId: data?.orderId });
+      if (!order) {
+        return callback({
+          success: false,
+          message: "Order not found",
+        });
+      }
+
+      socket.join(data?.orderId); // Join a room with the order ID
+
+      callback({
+        success: true,
+        message: "Order found",
+        order,
+      });
+    } catch (error) {
+      console.error(error);
+      callback({
+        success: false,
+        message: "Failed to track order",
       });
     }
   });
